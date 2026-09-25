820d898dd24f8aade28cedb0a0528f0863842aea
cyannick056-spec <cyannick056@gmail.com>
2026-09-24 21:27:29 -0500

    Bump Android app to v0.2.2 alpha

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.cris.doamodgallery"
         minSdk = 28
         targetSdk = 35
-        versionCode = 3
-        versionName = "0.2.1-alpha"
+        versionCode = 4
+        versionName = "0.2.2-alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
